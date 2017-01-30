4cb69a99d6a6f07d16c0cc00085d3a6cb86b706a
Paul Annesley <paul@annesley.cc>
2017-01-30 22:28:13 +1100

    main.c: blink test

--- main.c
+++ main.c
@@ -1,2 +1,24 @@
+#include <avr/io.h>
+#include <stdint.h>
+#include <util/delay.h>
+
+struct blinker_t {
+  volatile uint8_t * port;
+  volatile uint8_t * ddr;
+  uint8_t mask;
+} blinker;
+
 int main() {
+  blinker.port = &PORTB;
+  blinker.ddr = &DDRB;
+  blinker.mask = 1 << 5;
+
+  *blinker.ddr |= blinker.mask;
+
+  while (1) {
+    *blinker.port |= blinker.mask;
+    _delay_ms(10);
+    *blinker.port &= ~blinker.mask;
+    _delay_ms(990);
+  }
 }
